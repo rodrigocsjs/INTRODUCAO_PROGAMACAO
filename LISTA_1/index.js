f19922c7943b0675d32842e4cd82d1bb120d2a0d
rodrigocsjs <rodrigocsjs@gmail.com>
2025-04-01 18:54:57 -0300

    CREATE indes.js

---
+++ LISTA_1/index.js
@@ -0,0 +1,254 @@
+// 1)
+{
+    let idade = 25;
+    console.log(idade);
+}
+//-------------------------------------------
+
+// 2)
+{
+    let nome = "Maria";
+    nome = nome + " jose"
+    console.log(nome)
+
+}
+//--------------------------------------------
+
+// 3)
+{
+    const PI = 3.14;
+    console.log(PI);
+}
+//--------------------------------------------
+
+
+// 4)
+{
+    const numeroMaximo = 100;
+    try {
+        numeroMaximo = 200;
+    } catch (erro) {
+        console.log(erro + " Constantes não pode ser alterada durante a execução");
+    }
+}
+//--------------------------------------------
+
+// 5)
+{
+    const x = 10, y = 5;
+    let resultado = x + y;
+    console.log(resultado);
+
+}
+//--------------------------------------------
+
+
+// 6)
+{
+    const x = 5, y = 3, z = 4;
+    let total = x * y + z;
+    console.log(total);
+
+}
+//--------------------------------------------
+
+
+// 7)
+{
+    const nomeProduto = "Caneta"
+    console.log(nomeProduto);
+
+}
+//--------------------------------------------
+
+// 8)
+{
+    const a = 10, b = 20;
+    let soma = a + b;
+    console.log(soma);
+
+}
+//--------------------------------------------
+
+// 9)
+{
+    const x = 100, y = 30;
+    let difer = x - y;
+    console.log(difer);
+
+}
+//--------------------------------------------
+
+
+// 10)
+{
+    const m = 4, n = 3;
+    let mul = m * n;
+    console.log(mul);
+
+}
+//--------------------------------------------
+
+
+// 11)
+{
+    const dividendo = 50, divisor = 10;
+    let div = dividendo / divisor;
+    console.log(div);
+
+}
+//--------------------------------------------
+
+
+// 12)
+{
+    const x = 15, y = 4;
+    let modulo = x % y;
+    console.log(modulo);
+
+}
+//--------------------------------------------
+
+// 13)
+{
+    let contador = 0;
+    contador++;
+    console.log(contador);
+}
+//--------------------------------------------
+
+
+// 14)
+{
+    let contador = 10;
+    contador--;
+    console.log(contador);
+}
+//--------------------------------------------
+
+
+// 15)
+{
+    const base = 2, expoente =3;
+    let exp = base ** expoente;
+    console.log(exp);
+}
+//--------------------------------------------
+
+
+// 16)
+{
+    const a =5 , b =2  , c =10;
+    let res = a * (b + c);
+     console.log(res);
+}
+//--------------------------------------------
+
+
+// 17)
+{
+    const a =10, b =5  , c =3;
+    let res= a + b * c;
+    console.log(res," calculado pela  priroidade da multiplicação  tem sobrte a soma");
+}
+//--------------------------------------------
+
+
+// 18)
+{
+    const a =10, b =5  , c =3;
+    let res= (a + b) * c;
+    console.log(res,); 
+}
+//--------------------------------------------
+
+
+// 19)
+{
+    const a =25, b = 6;
+    let res= a / b;
+    console.log(res, "tipo float"); 
+}
+//--------------------------------------------
+
+// 20)
+{
+    const cidade ="São Paulo";
+    console.log(cidade); 
+}
+//--------------------------------------------
+
+// 21)
+{
+    const ano = 2025;
+    console.log("Estamos em", ano ,".")
+}
+//--------------------------------------------
+
+// 22)
+{
+    const a =3, b = 4;
+    console.log(a + b); 
+}
+//--------------------------------------------
+
+// 23)
+{
+    const empresa = "TechCorp";
+    let funcionario ="Lucas";
+console.log("O funcionário", funcionario ," trabalha na empresa" ,empresa,".")
+}
+//--------------------------------------------
+
+// 24)
+{
+    console.log(`O resultado da soma é , ${5 + 3}`);
+}
+//--------------------------------------------
+
+
+// 25)
+{
+    console.log(10 *( 2  +3) );
+}
+//--------------------------------------------
+
+
+// 26)
+{
+   let nome = "Carlos";
+   let idade =  30;
+   console.log(nome,"tem",idade,"anos.");
+}
+//--------------------------------------------
+
+// 27)
+{
+    console.log( 5 * 4 + 10 - 3);
+ }
+ //--------------------------------------------
+
+
+ // 28)
+{
+    console.log( 80 / 4);
+ }
+ //--------------------------------------------
+
+
+ // 29)
+{
+    let verificado = true;
+    if(verificado)
+    {
+        console.log("A verificação foi realizada com sucesso.");
+    }
+ }
+ //--------------------------------------------
+
+
+// 30)
+{
+    console.log((10 > 5) && (3 < 7))
+ }
+ //--------------------------------------------
